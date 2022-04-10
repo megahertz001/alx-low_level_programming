767a363fbdad91122ff06d87dcea6a8895eacac2
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-04-09 23:52:15 -0700

    combinations of two two-digits numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,9 +14,9 @@ int main(void)
 			if (i < j)
 			{
 				putchar((i / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((i / 10) + 48);
+				putchar((j/ 10) + 48);
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
