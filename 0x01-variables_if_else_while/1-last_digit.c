8e307cf76bf5cfbee1c23071d1cb025240f8e8a0
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-04-09 20:58:45 -0700

    a programme that tells if a number is greater that 5 or less thand 6 or zero

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - gets a random number
+ * execute and print
+ * Return: zero always
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int l = n % 10;
+	printf("Last digit of %d is %d ", n, l);
+	if (l > 5)
+		printf("and is greater than 5\n");
+	else if (l == 0)
+		printf("and is 0\n");
+	else if (l < 6 && l != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
+}
