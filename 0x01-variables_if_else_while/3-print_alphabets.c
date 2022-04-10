69322332f93ff85c782eb67d9f63e581e875c0f9
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-04-09 21:19:14 -0700

    prints lowercase letter a-z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints both lower and upper case letters a-z
+ * Returns: zero always
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch= 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
