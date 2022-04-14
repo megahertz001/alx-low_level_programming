36431dbdf53c6c3eda10687f20629a5d3a19e167
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-04-13 19:23:16 -0700

    check if a letter is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 
 /**
  * _islower - check if a char is lower case
+ * @c: c is an ascii character
  * Return: 1 if lower
  */
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
