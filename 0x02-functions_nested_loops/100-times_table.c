01e81baf98042554b055efaa0e4429fa1a8d1695
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-04-13 21:05:40 -0700

    print times table of a number

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 	{
 		for (row = 0; row <= n; row++)
 		{
-			for (row = 0; column <= n; column++)
+			for (column = 0; column <= n; column++)
 			{
 				product = row * column;
 
