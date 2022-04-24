5df4d5875433e581b076987433662741bf28f029
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-04-23 18:24:32 -0700

    hotfix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,22 +7,21 @@
 
 char *leet(char *s)
 {
-	int count = 0, i;
-	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
+	int i, ii;
+	char low_letters[] = "aeotl";
+	int upp_letters[] = "AEOTL";
+	int numbers[] = "43071";
 
-	while (*(s + count) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; i < 5; i++)
+		for (ii = 0; ii < 5; ii++)
 		{
-			if (*(s + count) == low_letters[i] || *(s + count) == up_letters[i])
+			if (s[i] == low_letters[ii] || s[i] == upp_letters[ii])
 			{
-				*(s + count) = numbers[i];
+				s[i] = numbers[ii];
 				break;
 			}
 		}
-		count++;
 	}
 
 	return (s);
