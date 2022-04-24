d010c07feb767265a96de00f017560189d75c19a
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-04-23 18:56:15 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,19 +8,29 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
 	}
 
-	if (n1 / 10 != 0)
+	d = m;
+	count = 1;
+
+	while (d > 9)
 	{
-		print_number(n1 / 10);
+		d /= 10;
+		count *= 10;
 	}
-	_putchar((n1 % 10) + '0');
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
