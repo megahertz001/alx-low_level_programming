44f05d1d2c61cef98a61d78cb948a7c264ee4fb2
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-05-05 20:09:00 -0700

    print execution file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: argunment count
+ * @argv: array of argument
+ * Return: 0 always
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
