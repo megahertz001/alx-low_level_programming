a1fcc1ba423f7ce7e4035abea3bf52291ff6bf8a
Israel Akintola Oluwafemi <oluwafemiisraelakintola@gmail.com>
2022-05-07 00:36:07 -0700

    malloc with c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a pointer to 2D array of integers
+ * @width: width of array
+ * @height: height of th4e array
+ * Return: pointer of an array of int
+ */
+int **alloc_grid(int width, int height)
+{
+	int **gridout;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
+	{
+		free(gridout);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+
+	return (gridout);
+}
+
